a21e6dfebcbee1efab051d443f3379c4282c94ec
Kobby Jones <kobbyj099@gmail.com>
2023-10-30 04:10:25 +0000

    Styled the form with crispy-bootstrap5

--- config/config/settings.py
+++ config/config/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     # Local Apps
     'heart_disease',
     'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- config/heart_disease/forms.py
+++ config/heart_disease/forms.py
@@ -1,7 +1,41 @@
 from django import forms
 from .models import HeartDiseasePrediction
+from crispy_forms.layout import Layout, Submit, Row, Column
+from crispy_forms.helper import FormHelper
 
 class HeartDiseasePredictionForm(forms.ModelForm):
     class Meta:
         model = HeartDiseasePrediction
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(HeartDiseasePredictionForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column('Age', css_class='form-group col-lg-6'),
+                Column('Sex', css_class='form-group col-lg-6'),
+            ),
+            Row(
+                Column('Chest pain type', css_class='form-group col-md-6'),
+                Column('Trestbps', css_class='form-group col-md-6'),
+            ),
+            Row(
+                Column('Cholestrol level', css_class='form-group col-md-6'),
+                Column('Fbs', css_class='form-group col-md-6'),
+            ),
+            Row(
+                Column('Restecg', css_class='form-group col-md-6'),
+                Column('Thalac', css_class='form-group col-md-6'),
+            ),
+            Row(
+                Column('Exang', css_class='form-group col-md-6'),
+                Column('Oldpeak', css_class='form-group col-md-6'),
+            ),
+            Row(
+                Column('Slope', css_class='form-control col-md-4'),
+                Column('Ca', css_class='form-group col-md-4'),
+                Column('Thal', css_class='form-group col-md-4'),
+            ),
+            Submit('submit', 'Predict', css_class='btn btn-primary')
+        )
