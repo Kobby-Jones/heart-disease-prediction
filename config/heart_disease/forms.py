2a747d10f4449d1acb51e7bdf34e63aae869aae8
Kobby Jones <kobbyj099@gmail.com>
2023-11-01 09:22:18 +0000

    Deleted Unused Functions

--- config/heart_disease/forms.py
+++ config/heart_disease/forms.py
@@ -8,29 +8,5 @@ class HeartDiseasePredictionForm(forms.ModelForm):
         model = HeartDiseasePrediction
         exclude = ['prediction_results']
 
-    helper = FormHelper()
-    helper.layout = Layout(
-        Row(
-            Column('Age', css_class='form-group col-lg-4'),
-            Column('Sex', css_class='form-group col-lg-4'),
-            Column('Chest pain type', css_class='form-group col-lg-4'),
-        ),
-        Row(
-            Column('Trestbps', css_class='form-group col-lg-4'),
-            Column('Cholestrol level', css_class='form-group col-lg-4'),
-            Column('Fbs', css_class='form-group col-lg-4'),
-        ),
-        Row(
-            Column('Restecg', css_class='form-group col-lg-4'),
-            Column('Thalac', css_class='form-group col-lg-4'),
-            Column('Exang', css_class='form-group col-lg-4'),
-        ),
-        Row(
-            Column('Oldpeak', css_class='form-group col-lg-3'),
-            Column('Slope', css_class='form-group col-lg-3'),
-            Column('Ca', css_class='form-group col-lg-3'),
-            Column('Thal', css_class='form-group col-lg-3'),
-        ),
-
-    )
+   
     
