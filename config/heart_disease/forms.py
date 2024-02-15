93d9d1afa7314991723cd0b870a914a57fa5c0f1
Kobby Jones <kobbyj099@gmail.com>
2024-02-15 03:48:44 +0000

    Styled the register page

--- config/heart_disease/forms.py
+++ config/heart_disease/forms.py
@@ -14,6 +14,15 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
+    def __init__(self, *args, **kwargs):
+         
+        super().__init__(*args, **kwargs)
+        self.fields['username'].help_text = ''
+        self.fields['username'].label = False
+        self.fields['username'].widget.attrs.update({'placeholder':'Username'})
+        self.fields['password1'].help_text = ''
+        self.fields['password1'].help_text = ''
+
 
 class LoginForm(AuthenticationForm):
     class Meta:
